2cc3efe7700b866ab341e73bbf0cde605ffd014e
Wenjie Sun <javenson0769@gmail.com>
2013-01-29 21:47:24 +0800

    base

---
+++ src-demo/com/dreamone/dev/MapActivity.java
@@ -0,0 +1,12 @@
+package com.dreamone.dev;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_activity_layout);
+    }
+}
